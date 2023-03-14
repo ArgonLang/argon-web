e7ca7a837a7c6653d139795f30a03073173124f9
jacopodl <jacopo.delu@gmail.com>
2023-03-14 10:33:23 +0100

    WIP: Homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,16 +10,38 @@ import styles from './index.module.css';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx('hero hero--primary', styles.heroBanner)} style={{flexDirection:"column", alignItems:"inherit", paddingBottom: 0}}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+        <div className="row" style={{display:"flex", alignItems:"center", }}>
+          <div className="col">
+            <table style={{textAlign: "left"}}>
+              <tbody>
+                <tr style={{border: "none"}}>
+                  <td style={{border: "none"}}>
+                    <h1>The Argon Language</h1>
+                    <p style={{fontSize:"1.2rem"}}>
+                      The Argon language is a work-in-progress interpreted multi-paradigm programming language. 
+                      Its syntax is influenced by many modern languages and aims to be <strong>elegant</strong>, <strong>clean</strong> and <strong>simple to use</strong>.
+                    </p>
+                    <button style={{ backgroundColor: "#d08770"}} className="button button--primary">Latest release »</button>
+                    &nbsp;
+                    <button className="button button--secondary">Source code</button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="col col-4">
+            <img src="/img/logo.svg" style={{height: "350px", width: "350px"}}/>
+          </div>
+        </div>
+      </div>
+      <div style={{display: "flex", flexDirection: "column"}}>
+      <img src="/img/waves.svg" style={{width: "100%"}}/>
+        <div style={{background:"#40495a"}}>
+          <code style={{border: "none",   backgroundColor:"transparent"}}>
+            import io
+          </code>
         </div>
       </div>
     </header>
@@ -30,7 +52,7 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
