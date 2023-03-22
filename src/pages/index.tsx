b95e6f13dafeaf097489b2e4cac01cc1b203d35d
Jacopo De Luca <jacopodl@users.noreply.github.com>
2023-03-22 17:02:53 +0100

    fix: meta description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,8 +57,8 @@ export default function Home(): JSX.Element {
 
   return (
     <Layout
-      title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={siteConfig.title}
+      description={siteConfig.tagline}>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
